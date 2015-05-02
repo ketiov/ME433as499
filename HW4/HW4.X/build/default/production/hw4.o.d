e91fb234b1d2cee399c4cbdddabc2e191510b842
ketiov <wojciechstojko2015@u.northwestern.edu>
2015-05-02 15:15:21 -0500

    hw4 and 5 from two weeks ago

---
+++ HW4/HW4.X/build/default/production/hw4.o.d
@@ -0,0 +1,4 @@
+build/default/production/hw4.o:  \
+hw4.c  \
+i2c_display.h  \
+i2c_master_int.h 
---
+++ HW5/HW5/build/default/production/hw5.o.d
@@ -0,0 +1,5 @@
+build/default/production/hw5.o:  \
+hw5.c  \
+i2c_display.h  \
+i2c_master_int.h  \
+ accel.h 
